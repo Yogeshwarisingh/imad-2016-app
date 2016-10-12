114e8cf94ad029cd659f37ae0fefbb3740bff052
Yogeshwarisingh <yashika.singh65@gmail.com>
2016-10-12 09:31:32 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,67 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleOne={
+    title: "Article-one Title",
+    heading: "Article-one",
+    date: "Sep 5,106",
+    content:  `<p>
+           The extraction process went smoothly.  I have it on my desktop ready for use.  I will let you know if I come across any issues.
+           </p>
+       </div>
+        <div>
+           <p>
+           The extraction process went smoothly.  I have it on my desktop ready for use.  I will let you know if I come across any issues.
+           </p>
+       </div>
+        <div>
+           <p>
+          The extraction process went smoothly.  I have it on my desktop ready for use.  I will let you know if I come across any issues.
+           </p>`
+    };
+    
+    function createtemplate(data)
+    {
+        tiltle=data.title;
+        heading=data.heading;
+        date=data.date;
+        content=data.content;
+        
+    var htmlTemplate=` 
+    <html>
+<head>
+    <title>
+    ${title}
+    </title>
+    <meta name="viewport" content="width-device-width,initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+    <body>
+        <div class="container">
+       <div>
+        <a href="/"></a>   
+       </div>
+       <hr/>
+       <h3>
+           ${heading}
+       </h3>
+       <div>
+           ${date}
+       </div>
+       <div>
+      ${content}
+       </div>
+       </div>
+    </body>
+</html>
+
+    `;
+    return htmlTemplate;
+    }
+
 app.get('/article-one',function(req, res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res)
