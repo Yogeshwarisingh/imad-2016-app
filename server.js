bab7dc97e8e010adbd530474f992589bdf15ceda
Yogeshwarisingh <yashika.singh65@gmail.com>
2016-10-15 07:17:09 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,6 +122,11 @@ app.get('/article-three',function(req, res)
    res.send("Articlie three is requested and will be send here"); 
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
